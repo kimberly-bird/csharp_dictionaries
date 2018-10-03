7b91bb445a780e57aadf21f86f09944371b08759
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-10-03 15:07:49 -0500

    updated with comments

--- Program.cs
+++ Program.cs
@@ -8,13 +8,16 @@ namespace dictionaries
     {
         static void Main(string[] args)
         {
+            // Create a simple dictionary with ticker symbols and company names
             Dictionary<string, string> stocks = new Dictionary<string, string>();
+
             stocks.Add("GM", "General Motors");
             stocks.Add("CAT", "Caterpillar");
             stocks.Add("AAPL", "Apple");
             stocks.Add("GOOG", "Google");
             stocks.Add("AMZN", "Amazon");
 
+            // Create list of ValueTuples that represents stock purchases. Properties will be ticker, shares, price.
             List<(string ticker, int shares, double price)> purchases = new List<(string, int, double)>();
 
             purchases.Add((ticker: "GM", shares: 150, price: 23.21));
@@ -33,13 +36,16 @@ namespace dictionaries
             purchases.Add((ticker: "AMZN", shares: 10, price: 25.21));
             purchases.Add((ticker: "AMZN", shares: 62, price: 14.87));
 
+            // Create a total ownership report that computes the total value of each stock that you have purchased.
             Dictionary<string, double> stockDetails = new Dictionary<string, double>();
 
             foreach ((string ticker, int shares, double price) purchase in purchases)
             {
-                // Does the company name key already exist in the report dictionary?
+                // companyName will be the key, stockValue will be the value in the dictionary
                 string companyName = stocks[purchase.ticker];
                 double stockValue = purchase.shares * purchase.price;
+
+                // Does the company name key already exist in the report dictionary?
                 // returns boolean for whether companyName key exists in stockDetails 
                 if (!stockDetails.ContainsKey(companyName))
                 {
@@ -54,7 +60,7 @@ namespace dictionaries
             // iterate over dictionary to display company and total stock value
             foreach(KeyValuePair<string, double> companyDetails in stockDetails)
             {
-            Console.WriteLine("{0}: {1}", companyDetails.Key, companyDetails.Value);
+            Console.WriteLine($"{companyDetails.Key}: {companyDetails.Value}");
             }
         }
     }
